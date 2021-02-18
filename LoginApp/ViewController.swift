f10e21344e22212992034b89bf85144be035b658
Dilshan <130susith@gmail.com>
2021-02-18 19:49:20 +0530

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Dilshan Jayasekara on 2/18/21.
+//  Copyright © 2021 Dilshan Jayasekara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
